abfabdcca8b4430b272525bc911edf1fad018a19
Marcus Stigelid <marcus.stigelid@devies.se>
2023-11-27 14:23:12 +0100

    Day 5

---
+++ day05/day05p1.py
@@ -0,0 +1,42 @@
+import argparse
+import os.path
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+def calculate(s: str):
+    current_stack, instructions = s.split('\n\n')
+
+    lastline_len = len(current_stack.splitlines()[-1].rstrip())
+    stacks: list[list[str]]
+    stacks = [[] for _ in range((lastline_len + 2) // 4)]
+
+    for line in current_stack.splitlines():
+        for i, c in enumerate(line[1::4]):
+            if not c.isspace():
+                stacks[i].append(c)
+
+    for stack in stacks:
+        stack.reverse()
+
+    for instruction in instructions.splitlines():
+        _, number_to_move, _, from_stack, _, destination_stack = instruction.split()
+        n, f, d = int(number_to_move), int(from_stack), int(destination_stack)
+
+        for _ in range(n):
+            stacks[d - 1].append(stacks[f - 1].pop())
+
+    return ''.join(stack[-1] if stack else '' for stack in stacks)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(calculate(f.read()))
+
+    return 0
+
+if __name__ == "__main__":
+    raise SystemExit(main())
